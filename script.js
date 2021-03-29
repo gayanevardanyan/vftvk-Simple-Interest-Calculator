1d297ddb554b1a519c0ce0cf4aff3cb4779b9c96
gayanevardanyan <vargaya83@yahoo.com>
2021-03-29 11:35:31 -0400

    update changes

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
+function checkdata(){
+    //Create references to the input elements we wish to validate
+    var principal = document.getElementById("principal");
+
+    //Check if principal field is empty
+    if(principal.value == "" || principal.value<=0){
+          alert("Enter a positive number");
+          principal.focus();
+          return false;
+    }
+   //If all is well return true.
+    return true;
+}
+
+function readRange(interest){
+    document.getElementById("interestSelected").innerHTML = interest.value +" %";
+}
+
+function add_years(dt,n) 
+ {
+ return new Date(dt.setFullYear(dt.getFullYear() + n));      
+ }
+
+function actualYear(years){
+dt = new Date();
+return add_years(dt, years).getFullYear();
+}
 function compute()
 {
-    p = document.getElementById("principal").value;
+    if(checkdata()){
+        var principal = document.getElementById("principal").value;
+        var rate = document.getElementById("rate").value;
+        var years = parseInt(document.getElementById("years").value);
     
+        var interest = principal * years * rate / 100;
+        //var interestValue = document.getElementById("interest").value;
+        readRange(document.getElementById("interest"));
+    
+        document.getElementById("result").innerHTML = "If you deposit <strong>" +principal+"</strong>,<br />"+
+        "at an interest rate of <strong>"+rate+" %</strong>.<br />"+
+        "You will receive an amount of <strong>"+interest+"</strong>,<br />"+
+        "in the year <strong>"+actualYear(years)+"</strong>";
+    }
+
 }
+
+
         
